3ab2e353684d5a3a6ef7d07f3cbe4346045ce962
Razvan Pricop <22615594+RazorBest@users.noreply.github.com>
2026-01-16 19:49:00 +0100

    reformat source code

--- main.py
+++ main.py
@@ -20,159 +20,107 @@ for spec_type in spec_type_tokens:
 # -------------- END HELPER FUNCTIONS FOR THE GRAMMAR -------------------------
 
 
-def unsigned_integer():
-    return _(r'\d+')
+def unsigned_integer(): return _(r'\d+')
 
+def n_keys(): return unsigned_integer
 
-def n_keys():
-    return unsigned_integer
+def num(): return unsigned_integer
 
-
-def num():
-    return unsigned_integer
-
-
-def hex_str():
-    return _(r'[0-9a-fA-F]+')
-
+def hex_str(): return _(r'[0-9a-fA-F]+')
 
 # Base58check encoded
 # Address starts with 1 for mainnet, and with n/m for testnet
 # All of them have 34 characters
-def p2pkh_address():
-    return _(r'(1|n|m)[1-9A-HJ-NP-Za-km-z]{33}')
-
+def p2pkh_address(): return _(r'(1|n|m)[1-9A-HJ-NP-Za-km-z]{33}')
 
 # Base58check encoded
 # Address starts with 3 for mainnet, and with n/m for testnet
 # Mainnet adress has 34 characters
 # Testnet adress has 35 characters
-def p2sh_address():
-    return [_(r'3[1-9A-HJ-NP-Za-km-z]{33}'), _(r'2[1-9A-HJ-NP-Za-km-z]{34}')]
-
+def p2sh_address(): return [_(r'3[1-9A-HJ-NP-Za-km-z]{33}'), _(r'2[1-9A-HJ-NP-Za-km-z]{34}')]
 
 # Bech32 or Bech32m encoded
 # All of them have 42 characters
-def p2wpkh_address():
-    return _(r'(bc|tb)1[01-9ac-hj-np-z]{39}')
-
+def p2wpkh_address(): return _(r'(bc|tb)1[01-9ac-hj-np-z]{39}')
 
 # Bech32 or Bech32m encoded
 # All of them have 62 characters
-def p2wpsh_address():
-    return _(r'(bc|tb)1[01-9ac-hj-np-z]{59}')
-
+def p2wpsh_address(): return _(r'(bc|tb)1[01-9ac-hj-np-z]{59}')
 
 # Bech32 or Bech32m encoded
 # All of them have 62 characters
-def p2tr_address():
-    return _(r'(bc|tb)1[01-9ac-hj-np-z]{59}')
+def p2tr_address(): return _(r'(bc|tb)1[01-9ac-hj-np-z]{59}')
 
+def segwit_address(): return [p2wpkh_address, p2wpsh_address, p2tr_address]
 
-def segwit_address():
-    return [p2wpkh_address, p2wpsh_address, p2tr_address]
-
-
-def addr_spec():
-    return [p2pkh_address, p2sh_address, segwit_address]
+def addr_spec(): return [p2pkh_address, p2sh_address, segwit_address]
 
 
 # -------------------- START RULES FOR KEY ------------------------------------
 
-def key_fingerprint():
-    return _(r'[0-9a-fA-F]{8}')
+def key_fingerprint(): return _(r'[0-9a-fA-F]{8}')
 
-
-def key_origin():
-    return '[', key_fingerprint, ZeroOrMore('/', num, Optional(["'", 'h'])), ']'
-
+def key_origin(): return '[', key_fingerprint, ZeroOrMore('/', num, Optional(["'", 'h'])), ']'
 
 # Constrained by KeyPublicHexUncompressedVisitor
-def key_public_hex_uncompressed():
-    return ['04'], _(r'[0-9a-fA-F]{130}')
-
+def key_public_hex_uncompressed(): return ['04'], _(r'[0-9a-fA-F]{130}')
 
 # Constrained by KeyPublicHexXOnlyVisitor
-def key_public_hex_xonly():
-    return _(r'[0-9a-fA-F]{64}')
+def key_public_hex_xonly(): return _(r'[0-9a-fA-F]{64}')
 
+def key_public_hex_compressed(): return ['02', '03'], key_public_hex_xonly
 
-def key_public_hex_compressed():
-    return ['02', '03'], key_public_hex_xonly
-
-
-def key_public_hex():
-    return [key_public_hex_compressed, key_public_hex_uncompressed]
-
+def key_public_hex(): return [key_public_hex_compressed, key_public_hex_uncompressed]
 
 # Base58
 # Constrained by KeyPrivateWifVisitor
-def key_private_wif():
-    return _(r"[1-9A-HJ-NP-Za-km-z]{51,52}")
-
+def key_private_wif(): return _(r"[1-9A-HJ-NP-Za-km-z]{51,52}")
 
 # Base58. Total length must be 111
 # Constrained by KeyStartB58Visitor
-def key_start_b58():
-    return ['xpub', 'xpriv', 'tpub', 'tpriv'], _(r"[1-9A-HJ-NP-Za-km-z]{107}")
+def key_start_b58(): return ['xpub', 'xpriv', 'tpub', 'tpriv'], _(r"[1-9A-HJ-NP-Za-km-z]{107}")
 
+def key_simple_path(): return ZeroOrMore('/', num, Optional(["'", 'h']))
 
-def key_simple_path():
-    return ZeroOrMore('/', num, Optional(["'", 'h']))
-
-
-def key_multibranch():
-    return '<', num, ZeroOrMore(';', num), '>'
-
+def key_multibranch(): return '<', num, ZeroOrMore(';', num), '>'
 
 def key_extended_path():
     return key_simple_path, Optional(key_multibranch), key_simple_path, Optional(['/*', "/*'", '/*h'])
 
-
-def key_extended():
-    return key_start_b58, key_extended_path
-
+def key_extended(): return key_start_b58, key_extended_path
 
 def key_no_uncompressed():
     return Optional(key_origin), [key_public_hex_compressed, key_extended, key_private_wif]
 
-
-def key():
-    return Optional(key_origin), [key_public_hex, key_extended, key_private_wif]
+def key(): return Optional(key_origin), [key_public_hex, key_extended, key_private_wif]
 
 # -------------------- END RULES FOR KEY --------------------------------------
 
 
 # -------------------- START RULES FOR TAPROOT --------------------------------
+
+def key_sequence_xonly():
+    return [key, key_public_hex_xonly], ZeroOrMore(',', [key, key_public_hex_xonly])
+
 def multi_key_tr():
    return [
         multi_a(n_keys, ',', key_sequence_xonly),
         sortedmulti_a(n_keys, ',', key_sequence_xonly),
     ]
 
-
 def tree():
     return [[pk([key, key_public_hex_xonly]), multi_key_tr], ('{', tree, ',', tree, '}')]
 
-def script_tr():
-    return tr([key, key_public_hex_xonly], Optional(',', tree))
+def taproot_script(): return tr([key, key_public_hex_xonly], Optional(',', tree))
 
 # -------------------- END RULES FOR TAPROOT ----------------------------------
 
 
 # -------------------- START RULES FOR SCRIPT ---------------------------------
 
-def script_key():
-    return [pk(key), pkh(key)]
+def script_key(): return [pk(key), pkh(key)]
 
-
-def key_sequence():
-    return key, OneOrMore(',', key)
-
-
-def key_sequence_xonly():
-    return [key, key_public_hex_xonly], OneOrMore(',', [key, key_public_hex_xonly])
-
+def key_sequence(): return key, ZeroOrMore(',', key)
 
 def multi_key():
    return [
@@ -180,26 +128,22 @@ def multi_key():
         sortedmulti(n_keys, ',', key_sequence),
     ]
 
+def subscript(): return [script_key, multi_key]
 
-def subscript():
-    return [script_key, multi_key]
-
-
-def script():
+def topscript():
     return [
-        sh(subscript), sh(wsh(subscript)), sh(wpkh(key_no_uncompressed)), wsh(subscript), script_key, multi_key,
-        wpkh(key_no_uncompressed), combo(key), script_tr, addr(addr_spec), raw(hex_str), rawtr(key)
+        sh(subscript), sh(wsh(subscript)), sh(wpkh(key_no_uncompressed)), wsh(subscript),
+        wpkh(key_no_uncompressed), combo(key), addr(addr_spec), raw(hex_str), rawtr(key),
     ]
+
+def script(): return [topscript, subscript, taproot_script]
 
 # -------------------- END RULES FOR SCRIPT -----------------------------------
 
 
-def checksum():
-    return _(r'[0-9a-zA-Z]{8}')
+def checksum(): return _(r'[0-9a-zA-Z]{8}')
 
-
-def descriptor():
-    return script, Optional('#', checksum), EOF
+def descriptor(): return script, Optional('#', checksum), EOF
 
 
 class DescriptorVisitor(PTNodeVisitor):
@@ -267,21 +211,14 @@ class DescriptorVisitor(PTNodeVisitor):
 # -------------------- START CURVE KEY PARSER ---------------------------------
 
 # Constrained by FieldElemVisitor
-def field_elem():
-    return _(r'\d+')
-
+def field_elem(): return _(r'\d+')
 
 # Constrained by ECPointVisitor
-def ecpoint():
-    return field_elem, ',', field_elem
+def ecpoint(): return field_elem, ',', field_elem
 
-
-def curve_key_point():
-    return [ecpoint, field_elem], EOF
-
+def curve_key_point(): return [ecpoint, field_elem], EOF
 
 curve_key_parser = ParserPython(curve_key_point)
-
 
 class CurveKeyVisitor(PTNodeVisitor):
     def visit_field_elem(self, node, children):
@@ -304,13 +241,9 @@ class CurveKeyVisitor(PTNodeVisitor):
 
 # -------------------- START KEY PRIVATE WIF PARSER ---------------------------
 
-def privkey_hex():
-    return _("[0-9a-f]{64}")
+def privkey_hex(): return _("[0-9a-f]{64}")
 
-
-def privkey_hex_inwif():
-    return ['80', 'ef'], privkey_hex, Optional('01'), EOF
-
+def privkey_hex_inwif(): return ['80', 'ef'], privkey_hex, Optional('01'), EOF
 
 key_private_wif_parser = ParserPython(privkey_hex_inwif)
 
@@ -328,8 +261,7 @@ class KeyPrivateWifVisitor(PTNodeVisitor):
 
 # -------------------- START COMPRESSED KEY DATA PARSER -----------------------
 
-def compressed_key_data():
-    return [key_public_hex_compressed, ('00', privkey_hex)]
+def compressed_key_data(): return [key_public_hex_compressed, ('00', privkey_hex)]
 
 compressed_key_data_parser = ParserPython(compressed_key_data)
 
@@ -337,6 +269,7 @@ class CompressedKeyDataVisitor(KeyPrivateWifVisitor):
     pass
 
 # -------------------- END COMPRESSED KEY DATA PARSER -------------------------
+
 
 def main():
     data = 'sh(pkh(03f6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5))'
@@ -346,7 +279,9 @@ def main():
     visit_parse_tree(parse_tree, DescriptorVisitor(debug=True))
     print(parse_tree.tree_str())
 
+
 # ------------ START UTIL ------------------
+
 from collections import deque
 from functools import reduce
 from hashlib import sha256
@@ -365,10 +300,8 @@ GENERATOR_ORDER = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD036
 def is_curve_point(x: int, y: int):
     return (y**2) % FIELD_ORDER == (x**3 + CURVE_A*x + CURVE_B)
 
-
 def is_field_element(x: int):
     return 0 <= x < FIELD_ORDER
-
 
 def is_exponent_element(x: int):
     return 0 <= x < GENERATOR_ORDER
@@ -380,7 +313,6 @@ BITCOIN_ALPHABET = b'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
 def _get_base58_decode_map(alphabet: bytes):
     return {ch: idx for idx, ch in enumerate(alphabet)}
-
 
 def b58decode_int(v: bytes) -> int:
     """
